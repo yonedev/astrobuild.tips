8b16572eb77af9968ceb1f8b9033e29c2f39dca1
Yoné Rocha <yone@yonerocha.net>
2023-07-21 21:49:25 +0100

    [ADD] React

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: true
-  }), mdx()]
+  }), mdx(), react()]
 });

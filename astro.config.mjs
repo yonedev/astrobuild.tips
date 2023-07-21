e1d69f44c5c7c95f11223dfb88e2edde55b21f47
Yoné Rocha <yone@yonerocha.net>
2023-07-21 22:17:04 +0100

    [ADD] vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: true
-  }), mdx(), react()]
+  }), mdx(), react(), vue()]
 });

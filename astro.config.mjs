aed8417a20afd5aa006c21cfc05bb5c633c53ed0
Yoné Rocha <yone@yonerocha.net>
2023-07-21 22:42:15 +0100

    [ADD] Svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: true
-  }), mdx(), react(), vue()]
+  }), mdx(), react(), vue(), svelte()]
 });

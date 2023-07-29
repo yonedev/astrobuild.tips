256d1d7fa0bfc444aa267fd7cf2975e3ebca20df
Yoné Rocha <yone@yonerocha.net>
2023-07-29 22:12:15 +0100

    [UPDATE] Revisión contenido

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,29 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
-import react from '@astrojs/react';
-import preact from "@astrojs/preact";
-import vue from '@astrojs/vue';
-import svelte from '@astrojs/svelte';
-import sitemap from "@astrojs/sitemap";
-
-
+import { defineConfig } from 'astro/config'
+import yaml from '@rollup/plugin-yaml'
+import tailwind from '@astrojs/tailwind'
+import mdx from '@astrojs/mdx'
+import react from '@astrojs/react'
+import preact from '@astrojs/preact'
+import vue from '@astrojs/vue'
+import svelte from '@astrojs/svelte'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://localhost:3000',
-  integrations: [tailwind({
-    applyBaseStyles: true
-  }), mdx(), react(), preact(), vue(), svelte(), sitemap()]
-});
+  compressHTML: true,
+  vite: {
+    plugins: [yaml()]
+  },
+  integrations: [
+    tailwind({
+      applyBaseStyles: true
+    }),
+    mdx(),
+    react(),
+    preact(),
+    vue(),
+    svelte(),
+    sitemap()
+  ]
+})
